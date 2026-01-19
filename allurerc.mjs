43779ec39be9b19bb4b79d5c7ba39fcfb7b0caa5
amielpeled <amielnoy@gmail.com>
2026-01-19 21:51:03 +0200

    define history in allure.mts

--- allurerc.mjs
+++ allurerc.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'allure';
 export default defineConfig({
   name: 'Playwright Test Report',
   output: './allure-report',
+  historyPath: './allure-history',
+  historyLimit: 20,
   plugins: {
     awesome: {
       options: {
